3d7d5bb524b6cf664fa44084d8f8c21fb2e56021
David Whitaker <damwhit@gmail.com>
2016-04-26 12:00:15 -0600

    uex is improved if search returns no results

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   def index
     if params[:product].include?(" ")
       @products = Product.all_by_long(params[:product])
+
     else
       @products = Product.all(params[:product])
     end
