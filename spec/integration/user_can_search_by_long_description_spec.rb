bff0d100a273eaab9d9a0949b518eabad9734a98
David Whitaker <damwhit@gmail.com>
2016-04-26 11:43:44 -0600

    working through edge cases

--- spec/integration/user_can_search_by_long_description_spec.rb
+++ spec/integration/user_can_search_by_long_description_spec.rb
@@ -1,13 +1,3 @@
-# As a user
-# When I visit the "/"
-# And I fill in the search box with "sennheiser headphones white" and click "search"
-# Then my current path should be "/search"
-# And I should see limited results (at the time of writing, 3. SKUs: 9068155, 9068004, 9068128)
-# And each result should contain sku, name, customer average review, short description, sale price, and image ONLY
-# ​
-# * The search should hit `https://api.bestbuy.com/v1/products` and results should be filtered on their `longDescription`. The search should return any product with the search term included.
-# * Display the returned products and their attributes returned from the API query in a logical and intuitive html layout. (This does not need to be overly styled).
-# * If the search yields no result, have an effective UI in place to inform the user.
 require 'rails_helper'
 
 RSpec.feature "user can search for products by long description" do
